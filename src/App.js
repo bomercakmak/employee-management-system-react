b4a6398bd09c7581b66b15008a255f05d51aa940
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-01-30 23:55:31 +0100

    I create the skeleton JSX and static employees state hooks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import EmployeeList from './components/EmployeeList';
 function App() {
   return (
     <div className="App">
-        <EmployeeList />
+
+      <div className="container-xl">
+        <div className="table-responsive">
+          <div className="table-wrapper">
+            <div className="table-title">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2><b>Employee Management System</b></h2>
+                </div>
+                <div className="col-sm-6">
+                  <a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+                </div>
+              </div>
+            </div>
+
+            <EmployeeList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
