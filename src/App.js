b7dcda29b8e960e49b3be81d7a934ff89668cafd
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-01 19:08:41 +0100

    I added React-Bootstrap. I added AddEmployee Form and Modal

--- src/App.js
+++ src/App.js
@@ -8,16 +8,6 @@ function App() {
       <div className="container-xl">
         <div className="table-responsive">
           <div className="table-wrapper">
-            <div className="table-title">
-              <div className="row">
-                <div className="col-sm-6">
-                  <h2><b>Employee Management System</b></h2>
-                </div>
-                <div className="col-sm-6">
-                  <a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
-                </div>
-              </div>
-            </div>
             < EmployeeContextProvider>
               <EmployeeList />
             </EmployeeContextProvider>
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,32 @@
+import {useState} from 'react'
 import {useContext} from 'react'
 import Employee from './Employee';
 import {EmployeeContext} from '../contexts/EmployeeContext';
+import { Button ,Modal} from 'react-bootstrap';
+import AddForm from './AddForm';
+
 
 const EmployeeList = () => {
 
       const {employees}  = useContext(EmployeeContext)
 
+      const [show,setShow] = useState(false)
+    
+      const handleClose = () => setShow(false)
+      const handleShow = () => setShow(true)
     return (
-
+        <>
+        <div className="table-title">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2><b>Employee Management System</b></h2>
+                </div>
+                <div className="col-sm-6">
+                  <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+                </div>
+              </div>
+            </div>
+       
         <table className="table table-striped table-hover">
             <thead>
                 <tr>
@@ -22,6 +41,21 @@ const EmployeeList = () => {
                 <Employee employees={employees} />
             </tbody>
         </table>
+
+       <Modal show={show} onHide={handleClose}>
+            <Modal.Header className="modal-header" closeButton>
+                <Modal.Title>
+                    Add New Employee
+                </Modal.Title>    
+            </Modal.Header>   
+            <Modal.Body>
+                <AddForm/>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}> Close </Button>
+            </Modal.Footer>
+        </Modal> 
+        </>
     )
 }
 
